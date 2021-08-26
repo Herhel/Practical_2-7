87dabc31c7ec15f6966da889376612e005e9c3fe
Herhel <herhelqa@gmail.com>
2021-08-26 16:28:20 +0300

    work

---
+++ src/com/alfa/Task27.java
@@ -0,0 +1,10 @@
+package com.alfa;
+
+import com.alfa.work123.Runner;
+
+public class Task27 {
+
+    public static void main(String[] args) {
+	new Runner().run();
+    }
+}
---
+++ src/com/alfa/work123/Runner.java
@@ -0,0 +1,7 @@
+package com.alfa.work123;
+
+public class Runner {
+    public void run(){
+
+    }
+}
